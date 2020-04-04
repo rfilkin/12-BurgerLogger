87ba419d101cbf2d8cb3619883dc80b3dafd5ef7
Robert Filkin <rfilkin17@gmail.com>
2020-04-03 22:25:09 -0700

    base functioning app. will need visual refinements

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whopper', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Double', true);
